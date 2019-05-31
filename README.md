7c1858c184c9a287aa704080eae6e11798c68f9d
冯文议 <xfsy_2015@163.com>
2019-05-31 15:42:59 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,1 @@
+# 学习Spring Boot集成MongoDB
---
+++ src/main/java/com/fengwenyi/learnspringbootmongodb/LearnSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.learnspringbootmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringbootMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringbootMongodbApplication.class, args);
+    }
+
+}
